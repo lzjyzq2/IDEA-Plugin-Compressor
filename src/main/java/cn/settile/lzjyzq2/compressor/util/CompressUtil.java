dfed2d20cb98a3d759b9118d01275fcf81fb0257
lzjyzq2 <lzjyzq2@hotmail.com>
2022-03-20 22:11:53 +0800

    add:增加CompressUtil工具类用于压缩与解压缩

---
+++ src/main/java/cn/settile/lzjyzq2/compressor/util/CompressUtil.java
@@ -0,0 +1,145 @@
+package cn.settile.lzjyzq2.compressor.util;
+
+import cn.settile.lzjyzq2.compressor.model.*;
+
+import javax.annotation.Nonnull;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class CompressUtil {
+
+    private final Map<String, Filter> filterCache = new HashMap<>();
+    private final Map<Filter, String> pathFilters = new HashMap<>();
+
+    private CompressorConfig compressorConfig = null;
+
+    public CompressUtil() {
+
+    }
+
+    public void use(CompressorConfig compressorConfig) {
+        this.compressorConfig = compressorConfig;
+        filterCache.clear();
+        pathFilters.clear();
+        convertFileNodes(compressorConfig.getSources().getFileNodes());
+        convertFolderNodes(compressorConfig.getSources().getFolderNodes());
+        convertFolderNodes(compressorConfig.getSources().getFolderNodes());
+        convertToPathFilters();
+    }
+
+    private void convertFileNodes(List<FileNode> fileNodes) {
+        if (fileNodes != null) {
+            fileNodes.forEach(fileNode -> {
+                Filter filter = new Filter();
+                filter.setParent(fileNode.getParent());
+                filter.setSources(filter.getSources());
+                filter.setRegex("^" + fileNode.getName() + "$");
+                filter.setId(fileNode.getId());
+                filter.setIncludesFile(true);
+                filter.setIncludesFolder(false);
+                filterCache.put(fileNode.getId(), filter);
+            });
+        }
+    }
+
+    private void convertFolderNodes(List<FolderNode> folderNodes) {
+        if (folderNodes != null) {
+            folderNodes.forEach(folderNode -> {
+                Filter filter = new Filter();
+                filter.setParent(folderNode.getParent());
+                filter.setSources(filter.getSources());
+                filter.setRegex("^" + folderNode.getName() + "$");
+                filter.setId(folderNode.getId());
+                filter.setIncludesFile(false);
+                filter.setIncludesFolder(true);
+                convertFileNodes(folderNode.getFileNodes());
+                convertFilters(folderNode.getFilters());
+                convertFolderNodes(folderNode.getFolderNodes());
+                filterCache.put(folderNode.getId(), filter);
+            });
+        }
+    }
+
+    private void convertFilters(List<Filter> filters) {
+        if (filters != null) {
+            filters.forEach(filter -> {
+                filterCache.put(filter.getId(), filter);
+            });
+        }
+    }
+
+    private void copyToTmpDir() {
+        if (compressorConfig == null) {
+            throw new RuntimeException("为找到配置");
+        }
+        String tmpDir = compressorConfig.getTmpDir();
+        if (tmpDir == null || tmpDir.isBlank()) {
+            tmpDir = System.getProperty("java.io.tmpdir") + Var.TEMP_FOLDER_NAME + File.separator;
+        }
+        File tmpDirFile = Paths.get(tmpDir).toFile();
+        if (!tmpDirFile.isDirectory()) {
+            throw new RuntimeException("\"" + tmpDir + "\"不是文件夹");
+        }
+        if (!tmpDirFile.exists()) {
+            if (!tmpDirFile.mkdir()) {
+                throw new RuntimeException("临时文件夹[" + tmpDir + "]创建失败");
+            }
+        }
+        if (!tmpDirFile.canWrite()) {
+            throw new RuntimeException("临时文件夹[" + tmpDir + "]无法写入");
+        }
+        // 开始拷贝
+
+    }
+
+    public List<Filter> getFiltersWithOut(Out out) {
+        List<Filter> filters = new LinkedList<>();
+        Filter filter = filterCache.get(out.getId());
+        String dirPath = getFilterDir(filter);
+        pathFilters.forEach(((filterKey, s) -> {
+            if (dirPath.contains(s)) {
+                filters.add(filterKey);
+            }
+        }));
+        return filters;
+    }
+
+    private void convertToPathFilters() {
+        pathFilters.clear();
+        filterCache.values().forEach(filter -> {
+            pathFilters.put(filter, getFilterDir(filter));
+        });
+    }
+
+    public String getFilterDir(Filter filter) {
+        return getFolderPath(filter.getParent());
+    }
+
+    public String getFileDir(FileNode fileNode) {
+        return getFolderPath(fileNode.getParent());
+    }
+
+    public String getFolderDir(FolderNode folderNode) {
+        return getFolderPath(folderNode.getParent());
+    }
+
+    public String getFolderPath(@Nonnull FolderNode folderNode) {
+        List<String> pathList = new LinkedList<>();
+        FolderNode tmp = folderNode;
+        while (tmp != null) {
+            pathList.add(tmp.getName());
+            tmp = tmp.getParent();
+        }
+        pathList.add(folderNode.getSources().getSrc());
+        Collections.reverse(pathList);
+        return String.join(File.separator, pathList);
+    }
+
+    public void release() {
+        filterCache.clear();
+        pathFilters.clear();
+        compressorConfig = null;
+    }
+}
